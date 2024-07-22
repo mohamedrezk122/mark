5b831f55aef262c5b2c92eae5dbb0ca861051ae8
Mohamed Rezk <mohrizq895@gmail.com>
2024-07-22 20:06:01 +0300

    Move entry point to cli file

--- mark/server.py
+++ mark/server.py
@@ -10,9 +10,11 @@ from mark.rofi import Rofi
 
 
 class Server:
-    def __init__(self, db_filename, mode, on_selection: Callable = None):
+    def __init__(
+        self, db_filename: str, mode: str = "read", on_selection: Callable | None = None
+    ):
         assert mode in ["read", "write"], "mode has to be 'read' or 'write'"
-        prompt = "Choose Dir"
+        prompt = "choose dir"
         self.mode = mode
         self.db = DataBase(db_filename)
         os.environ["ROFI_MODE"] = "write"
@@ -22,7 +24,7 @@ class Server:
         self.on_selection = on_selection
         self.parent_path = None  # selected parent dir
 
-    async def __handle_read_mode(self, reader, writer):
+    async def __handle_read_mode(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         while True:
             response = (await reader.read(500)).decode("utf8")
             # response -> {"code": return_code, "value": selected_item}
@@ -31,12 +33,10 @@ class Server:
             response = json.loads(response)
             # selected item is listed
             if response["code"] != 1:
-                continue 
+                continue
             if not self.db.is_dir(response["value"]):
                 # TODO: be presistant ?
-                name, url = self.db.get_bookmark(
-                    self.parent_path, response["value"]
-                )
+                name, url = self.db.get_bookmark(self.parent_path, response["value"])
                 self.on_selection(name, url)
                 writer.write("quit".encode("utf8"))
                 await writer.drain()
@@ -44,7 +44,7 @@ class Server:
             self.parent_path = response["value"]
             items = self.db.list_bookmarks(response["value"])
             kwargs = {
-                "prompt": "Choose bookmark",
+                "prompt": "choose bookmark",
             }
             data = self.rofi.update_data(items, **kwargs)
             writer.write(data)
@@ -52,7 +52,7 @@ class Server:
         writer.close()
         await writer.wait_closed()
 
-    async def __handle_write_mode(self, reader, writer):
+    async def __handle_write_mode(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         # TODO: implement bookmark insertion function
         raise NotImplementedError()
 
@@ -65,13 +65,19 @@ class Server:
             await server.serve_forever()
 
 
-def copy_selection(name, url):
+def copy_selection(name: str, url: str):
+    """
+    Copy selected bookmark's url to clipboard
+    """
     import pyperclip
 
     pyperclip.copy(url)
 
 
-def open_selection(name, url):
+def open_selection(name: str, url: str):
+    """
+    Open selected bookmark's url in default browser
+    """
     system = platform.system()
     if system == "Windows":
         os.startfile(url)
@@ -84,14 +90,3 @@ def open_selection(name, url):
         subprocess.run(["xdg-open", url])
     except OSError:
         print("Cannot open default browser")
-
-
-async def main():
-    async_server = Server("db.yaml", mode="read", on_selection=open_selection)
-    await asyncio.gather(
-        async_server.rofi.open_menu(async_server.db.list_dirs()),
-        async_server.run_server(),
-    )
-
-
-asyncio.run(main())
