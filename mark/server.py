2e8acd1d54ea292e7e482617f072c37d62b34439
Mohamed Rezk <mohrizq895@gmail.com>
2024-07-26 19:47:36 +0300

    Catch exceptions in handler coroutine

--- mark/server.py
+++ mark/server.py
@@ -1,27 +1,28 @@
 import asyncio
 import json
-import os
-import platform
-import subprocess
-from typing import Callable
 
-from mark.db import DataBase
+from tinydb import TinyDB
+
 from mark.rofi import Rofi
+from mark.utils import copy_selection, open_selection
 
 
 class Server:
     def __init__(
-        self, db_filename: str, mode: str = "read", on_selection: Callable | None = None
+        self,
+        port: int,
+        db: TinyDB,
+        mode: str = "read",
+        rofi_inst: Rofi = None,
+        on_selection: str = None,
     ):
         assert mode in ["read", "write"], "mode has to be 'read' or 'write'"
-        prompt = "choose dir"
         self.mode = mode
-        self.db = DataBase(db_filename)
-        os.environ["ROFI_MODE"] = self.mode
-        self.rofi = Rofi(prompt=prompt)
+        self.port = port
+        self.rofi = rofi_inst
+        self.db = db
         self.on_selection = on_selection
-        self.parent_path = None  # selected parent dir
-        # container for bookmark insertion
+        # container for selection tracking
         self.pack = {"current": 0}
 
     async def __close_connection(self, writer: asyncio.StreamWriter):
@@ -29,17 +30,20 @@ class Server:
         await writer.drain()
 
     async def __handle_root_selection(self, writer: asyncio.StreamWriter, root: str):
-        # TODO: be presistant ?
-        title, url = self.db.get_bookmark(self.parent_path, root)
-        self.on_selection(title, url)
+        on_selection_funcs = {
+            "copy": copy_selection,
+            "open": open_selection,
+        }
+        title, url = self.db.get_bookmark(self.pack["path"], root)
+        on_selection_funcs[self.on_selection](title, url)
         await self.__close_connection(writer)
 
     async def __handle_path_selection(self, writer: asyncio.StreamWriter, path: str):
         # TODO: handle hierarchical paths
-        self.parent_path = path
+        self.pack["path"] = path
         items = self.db.list_bookmarks(path)
         kwargs = {
-            "prompt": "choose bookmark",
+            "message": "choose bookmark",
         }
         data = self.rofi.update_data(items, **kwargs)
         writer.write(data)
@@ -53,13 +57,13 @@ class Server:
         self.pack[type_] = value
         if type_ == "title":
             return
-        kwargs = {
-            "prompt": "insert url or location",
-            # TODO: paste key
-            "message": "Paste with key",
-        }
+        state = "url"
         if type_ == "url":
-            kwargs["prompt"] = "insert title"
+            state = "title"
+        kwargs = {
+            # TODO: paste key
+            "message": f"insert {state} (paste with key {self.rofi.paste_key})",
+        }
         data = self.rofi.update_data(None, **kwargs)
         writer.write(data)
         await writer.drain()
@@ -82,50 +86,30 @@ class Server:
         self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter
     ):
         while True:
-            response = (await reader.read(500)).decode("utf8")
-            # response format -> {"code": return_code, "value": selected_item}
-            if response is None:
+            if self.rofi.server_should_exit:
+                quit()
+            try:
+                await asyncio.sleep(0)
+                response = await reader.read(500)
+                # response format -> {"code": return_code, "value": selected_item}
+                if response is None:
+                    continue
+                response = json.loads(response.decode("utf8"))
+                res_value = response["value"]
+                if self.mode == "read" and not self.db.is_dir(res_value):
+                    await self.__handle_root_selection(writer, res_value)
+                elif self.mode == "read":
+                    await self.__handle_path_selection(writer, res_value)
+                elif self.mode == "write":
+                    await self.__handle_bookmark_insertion(writer, res_value)
+            except:
                 continue
-            response = json.loads(response)
-            if self.mode == "read" and not self.db.is_dir(response["value"]):
-                await self.__handle_root_selection(writer, response["value"])
-            elif self.mode == "read":
-                await self.__handle_path_selection(writer, response["value"])
-            elif self.mode == "write":
-                await self.__handle_bookmark_insertion(writer, response["value"])
         writer.close()
         await writer.wait_closed()
 
     async def run_server(self):
         server = await asyncio.start_server(
-            self.__handle_readwrite_mode, host="localhost", port=15555
+            self.__handle_readwrite_mode, host="localhost", port=self.port
         )
         async with server:
             await server.serve_forever()
-
-
-def copy_selection(title: str, url: str):
-    """
-    Copy selected bookmark's url to clipboard
-    """
-    import pyperclip
-
-    pyperclip.copy(url)
-
-
-def open_selection(title: str, url: str):
-    """
-    Open selected bookmark's url in default browser
-    """
-    system = platform.system()
-    if system == "Windows":
-        os.startfile(url)
-        return
-    elif system == "Darwin":
-        subprocess.run(["open", url])
-        return
-    # Linux or BSD
-    try:
-        subprocess.run(["xdg-open", url])
-    except OSError:
-        raise RuntimeError("Cannot open default browser")
