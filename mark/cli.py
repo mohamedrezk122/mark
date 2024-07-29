58c900d86a89fb83fc9d98c2900064abe33770f1
Mohamed Rezk <mohrizq895@gmail.com>
2024-07-29 20:35:57 +0300

    feat: expose the import from browser functionality as a cli command

--- mark/cli.py
+++ mark/cli.py
@@ -2,16 +2,15 @@ import asyncio
 
 import click
 
-from mark.db import DataBase
-from mark.rofi import Rofi
-from mark.server import Server
-from mark.utils import get_free_port
+from mark.db import insert_multiple_bookmarks
+from mark.parser import parse_netscape_bookmark_file
+from mark.server import execute_async_server
 
 db_files_arg = click.argument(
     "db_files", required=True, nargs=-1, type=click.Path(exists=True)
 )
 output_file_opt = click.option(
-    "-o", "--output", type=click.Path(), default="merged.yaml", show_default=True
+    "-o", "--output", type=click.Path(), default="output.yaml", show_default=True
 )
 on_selection_opt = click.option(
     "--on-selection",
@@ -21,25 +20,18 @@ on_selection_opt = click.option(
     type=click.Choice(["copy", "open"], case_sensitive=False),
     multiple=False,
 )
-
-
-async def execute_async_server(db_filename: str, mode: str, on_selection: str = None):
-    if mode == "write":
-        import pyperclip
-
-        url = pyperclip.paste()
-    port = get_free_port()
-    message = "choose or create dir" if mode == "read" else "choose dir"
-    rofi = Rofi(message=message).setup_client(mode, port)
-    db = DataBase(db_filename)
-    async_server = Server(
-        port, db, mode=mode, rofi_inst=rofi, on_selection=on_selection, url=url
-    )
-
-    await asyncio.gather(
-        async_server.rofi.open_menu(async_server.db.list_dirs()),
-        async_server.run_server(),
-    )
+format_opt = click.option(
+    "-f",
+    "--format",
+    default="html",
+    show_default=True,
+    show_choices=True,
+    type=click.Choice(["html", "markdown"], case_sensitive=False),
+)
+bookmark_file_arg = click.argument(
+    "file",
+    type=click.Path(exists=True),
+)
 
 
 @click.group()
@@ -71,6 +63,22 @@ def mark_insert_bookmark(db_files):
     # TODO: handle multiple files
     db_files = db_files[0]
     asyncio.run(execute_async_server(db_files, "write"))
+
+
+@cli.command("import")
+@bookmark_file_arg
+@format_opt
+@output_file_opt
+def mark_import_bookmarks(file, format, output):
+    """
+    Import bookmarks from other browsers
+    """
+    if format == "html":
+        bookmarks = parse_netscape_bookmark_file(file)
+    else:
+        raise NotImplementedError()
+
+    insert_multiple_bookmarks(bookmarks, output)
 
 
 if __name__ == "__main__":
