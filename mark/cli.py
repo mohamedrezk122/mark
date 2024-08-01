943412d4935882a755b0e8fe065e1fad16db63ff
Mohamed Rezk <mohrizq895@gmail.com>
2024-08-01 13:01:58 +0300

    refactor: restrict db files to single entry and refactor import function to account for default output naming

--- mark/cli.py
+++ mark/cli.py
@@ -1,17 +1,15 @@
 import asyncio
 
 import click
+from click.core import ParameterSource
 
-from mark.db import insert_multiple_bookmarks
+from mark.db import save_bookmarks_to_db
 from mark.parser import parse_netscape_bookmark_file
 from mark.server import execute_async_server
 
-
-db_files_arg = click.argument(
-    "db_files", required=True, nargs=-1, type=click.Path(exists=True)
-)
+db_file_arg = click.argument("db_file", required=True, type=click.Path(exists=True))
 output_file_opt = click.option(
-    "-o", "--output", type=click.Path(), default="output.yaml", show_default=True
+    "-o", "--output", type=click.Path(), default="output", show_default=True
 )
 on_selection_opt = click.option(
     "--on-selection",
@@ -34,6 +32,16 @@ bookmark_file_arg = click.argument(
     type=click.Path(exists=True),
 )
 
+
+def is_default(param):
+    """
+    Return true if the click option is not passed to cmd, and the default value
+    is used
+    """
+    parameter_source = click.get_current_context().get_parameter_source(param)
+    return parameter_source == ParameterSource.DEFAULT
+
+
 @click.group()
 def cli():
     """
@@ -43,26 +51,24 @@ def cli():
 
 
 @cli.command("get")
-@db_files_arg
+@db_file_arg
 @on_selection_opt
-def mark_get_bookmark(db_files, on_selection):
+def mark_get_bookmark(db_file, on_selection):
     """
     Retrieve a bookmark
     """
     # TODO: handle multiple files
-    db_files = db_files[0]
-    asyncio.run(execute_async_server(db_files, "read", on_selection))
+    asyncio.run(execute_async_server(db_file, "read", on_selection))
 
 
 @cli.command("insert")
-@db_files_arg
-def mark_insert_bookmark(db_files):
+@db_file_arg
+def mark_insert_bookmark(db_file):
     """
     Insert a bookmark
     """
     # TODO: handle multiple files
-    db_files = db_files[0]
-    asyncio.run(execute_async_server(db_files, "write"))
+    asyncio.run(execute_async_server(db_file, "write"))
 
 
 @cli.command("import")
@@ -78,7 +84,10 @@ def mark_import_bookmarks(file, format, output):
     else:
         raise NotImplementedError()
 
-    insert_multiple_bookmarks(bookmarks, output)
+    if is_default("output"):
+        output = "import_bookmarks.json"
+
+    save_bookmarks_to_db(bookmarks, output)
 
 
 if __name__ == "__main__":
